1bfb5101c75df8adb61da51ffcb0251af7888b42
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 20:02:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  puts array.detect{|i| i.length == 4 }
+  puts array.select{|i| i.length == 4 }
 end
 
 def find_the_cheese# code an argument here
