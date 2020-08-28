5b43f5fa60b78103ab2d9114aa97e8774b69fe93
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 20:06:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,10 +18,10 @@ def long_planeteer_calls(array)
   array.each do |name|
     if name.length > 4
       return true
+    elsif name.length <= 4
+      return false
     end
   end
-
-  puts array.select{|i| i.length == 4 }
 end
 
 def find_the_cheese# code an argument here
