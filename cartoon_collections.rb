51d7d84ac0da25d24c2f71ee6760a23ab86f8986
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 20:16:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def long_planeteer_calls(array)
   return false if array.all?{|i| i.length <= 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return if array.find{|i| i == cheese}
+  end
 end
